fb6b7635764311a2f08c88dcca15166d3134bdff
Andreas Paepcke <paepcke@cs.stanford.edu>
2018-05-01 13:13:29 -0700

    Checkpoint

--- src/sankey_majors/sankey_majors.py
+++ src/sankey_majors/sankey_majors.py
@@ -24,7 +24,7 @@ class SankeyMajors(object):
     
     #--------------------------
     # __init__
-    #-------------------#--------------------------
+    #-------------------
 
     
     def __init__(self,
@@ -41,37 +41,11 @@ class SankeyMajors(object):
                             passwd=self.mysql_passwd, 
                             db=self.majors_table)
         
-        nodes = self.get_nodes()
         links = self.get_links()
+        nodes = self.get_nodes(links)
         SankeyDiagram.plot_sankey(nodes, 
                                   links, 
                                   plot_title="Majors Transitions")
-        
-    #--------------------------
-    # get_nodes 
-    #-------------------
-    
-    def get_nodes(self):
-        query = '''SELECT major_num, major
-                     FROM major_nums;
-                '''
-        # Get:
-        #     [ 
-        #       (1,AA-BS)
-        #       (2,AES-BAS)
-        #       (3,AES-BS)
-        #     ]
-        MAJOR_NUM  = 0
-        MAJOR_NAME = 1
-        
-        node_info = self.mydb.query(query).nextall()
-        nodes = [ SankeyNode(one_node_info[MAJOR_NUM],
-                             one_node_info[MAJOR_NAME],
-                             self.get_color(one_node_info[MAJOR_NAME])
-                             )
-                             for one_node_info in node_info 
-                             ]
-        return(nodes)
         
     #--------------------------
     # get_links 
@@ -90,13 +64,59 @@ class SankeyMajors(object):
         NUM_TRANSITIONS = 2
         
         link_info = self.mydb.query(query).nextall()
-        links = [ SankeyLink(one_link_info[MAJOR_LEFT_NUM],
-                             one_link_info[MAJOR_RIGHT_NUM],
-                             one_link_info[NUM_TRANSITIONS]
+        
+        # The int coercions below prevent nums to be treated
+        # like LONG, and have subsequent displays of the numbers
+        # have an 'L' suffix:
+        
+        links = [ SankeyLink(int(one_link_info[MAJOR_LEFT_NUM]),
+                             int(one_link_info[MAJOR_RIGHT_NUM]),
+                             int(one_link_info[NUM_TRANSITIONS])
                              )
                              for one_link_info in link_info
                  ]
         return(links)
+    
+    #--------------------------
+    # get_nodes 
+    #-------------------
+    
+    def get_nodes(self, sankey_link_obj_list):
+        
+        # The 'int' coercion is to avoid the suffix
+        # 'L'
+        nodes_to_get = [ link_obj.src_node_num for link_obj in sankey_link_obj_list ]
+        nodes_to_get.extend([ link_obj.target_node_num for link_obj in sankey_link_obj_list ])
+
+        # Turn into a tuple so that conversion to string
+        # in query below will yield a nice list of node numbers
+        # in parens:
+        
+        node_num_tuple = tuple(nodes_to_get)
+        
+        query = '''SELECT major_num, major
+                     FROM major_nums
+                    WHERE major_num IN %s;
+                ''' % str(node_num_tuple)
+        # Get:
+        #     [ 
+        #       (1,AA-BS)
+        #       (2,AES-BAS)
+        #       (3,AES-BS)
+        #     ]
+        MAJOR_NUM  = 0
+        MAJOR_NAME = 1
+        
+        node_info = self.mydb.query(query).nextall()
+        # The 'str()' is to get rid of the unicode 'u' prefix:
+        nodes = [ SankeyNode(int(one_node_info[MAJOR_NUM]),
+                             str(one_node_info[MAJOR_NAME]),
+                             self.get_color(one_node_info[MAJOR_NAME])
+                             )
+                             for one_node_info in node_info 
+                             ]
+        return(nodes)
+        
     
     #--------------------------
     # get_color 
@@ -104,104 +124,7 @@ class SankeyMajors(object):
     
     def get_color(self, major_name):
         return 'blue'
-    #--------------------------
-    # get_sources
-    #-------------------
-    
-    def get_sources(self):
-      
         
-        query = '''SELECT major_num_left, major AS major_name_left, count(*) AS num_students
-                    FROM majors_links LEFT JOIN majors_nodes
-                      ON majors_links.major_num_left = majors_nodes.node_num
-                   WHERE is_ug = 1
-                  GROUP BY major_num_left;
-                '''
-        node_num_pos = 0
-        node_majors_pos = 1
-        node_volume_pos = 2
-        sources_node_nums_names_and_volumes = self.mydb.query(query).nextall()
-        sources_nums = [sources_info[node_num_pos] for sources_info in sources_node_nums_names_and_volumes]
-        sources_majors  = [sources_info[node_majors_pos] for sources_info in sources_node_nums_names_and_volumes]
-        sources_volumes = [sources_info[node_volume_pos] for sources_info in sources_node_nums_names_and_volumes]
-        return (sources_nums, sources_majors, sources_volumes)
-        
-
-    #--------------------------
-    # get_targets
-    #-------------------
-    
-    def get_targets(self):
-      
-        
-        query = '''SELECT major_num_right, major AS major_right, count(*) AS num_students
-                    FROM majors_links LEFT JOIN majors_nodes
-                      ON majors_links.major_num_right = majors_nodes.node_num
-                   WHERE is_ug = 1
-                  GROUP BY major_num_right;
-                '''
-        node_num_pos = 0
-        node_majors_pos = 1
-        node_volume_pos = 2
-        targets_node_nums_names_and_volumes = self.mydb.query(query).nextall()
-        targets_nums = [targets_info[node_num_pos] for targets_info in targets_node_nums_names_and_volumes]
-        targets_majors  = [targets_info[node_majors_pos] for targets_info in targets_node_nums_names_and_volumes]
-        targets_volumes = [targets_info[node_volume_pos] for targets_info in targets_node_nums_names_and_volumes]
-        return (targets_nums, targets_majors, targets_volumes)
-
-    #--------------------------
-    # 
-    #-------------------
-    
-    def make_plot(self,
-                  source_nums,
-                  source_majors,
-                  sources_volumes,
-                  targets_nums,
-                  targets_majors,
-                  targets_volumes):
-
-        majors_nums   = source_nums   + targets_nums
-        majors_names  = source_majors + targets_majors
-        majors_colors = ['blue']*len(majors_nums)
-        
-        nodes = [ SankeyNode(node_info[0],
-                             node_info[1],
-                             node_info[2]
-                             ) 
-                             for node_info in zip(majors_nums, majors_names, majors_colors) 
-                             ]
-
-        links = [ SankeyLink(link_info[0],
-                             link_info[1],
-                             link_info[2]
-                             ) 
-                             for link_info in zip(source_nums, targets_nums, targets_volumes) 
-                             ]
-        
-        
-
-
-            
-    #--------------------------
-    # get_major_names 
-    #-------------------
-    
-    def get_major_names(self):
-      
-        
-        query = '''SELECT major_num_left, count(*) AS num_students
-                    FROM majors_links LEFT JOIN majors_nodes
-                      ON majors_links.major_num_left = majors_nodes.node_num
-                   WHERE is_ug = 1
-                  GROUP BY major_num_left;
-                '''
-        source_nodes_and_volumes = self.mydb.query(query).nextall()
-        sources = [sources_and_volumes[0] for sources_and_volumes in source_nodes_and_volumes]
-        volumes = [sources_and_volumes[1] for sources_and_volumes in source_nodes_and_volumes]
-        return (sources, volumes)
-
-          
     #--------------------------
     # get MySQLPasswd
     #-------------------
--- src/sankey_majors/sankey_tests.py
+++ src/sankey_majors/sankey_tests.py
@@ -7,23 +7,41 @@ import plotly
 
 
 if __name__ == '__main__':
-    data = dict(
-        type='sankey',
-        node = dict(
-          pad = 15,
-          thickness = 20,
-          line = dict(
-            color = "black",
-            width = 0.5
-          ),
-          label = ["A1", "A2", "B1", "B2", "C1", "C2"],
-          color = ["blue", "blue", "blue", "blue", "blue", "blue"]
-        ),
-        link = dict(
-          source = [0,1,0,2,3,3],
-          target = [2,3,3,4,4,5],
-          value = [8,4,2,8,4,2]
-      ))
+#     data = dict(
+#         type='sankey',
+#         node = dict(
+#           pad = 15,
+#           thickness = 20,
+#           line = dict(
+#             color = "black",
+#             width = 0.5
+#           ),
+#           label = ["A1", "A2", "B1", "B2", "C1", "C2"],
+#           color = ["blue", "blue", "blue", "blue", "blue", "blue"]
+#         ),
+#         link = dict(
+#           source = [0,1,0,2,3,3],
+#           target = [2,3,3,4,4,5],
+#           value = [8,4,2,8,4,2]
+#       ))
+
+    data = {
+          'type': 'sankey',
+          'node':  {
+                     'pad': 15,
+                     'thickness': 20,
+                     'line': {'color': 'black', 'width': 0.5},
+                     'label': ['CS-BS', 'ECON-BA', 'ENGL-BA', 'HSTRY-BA', 'INTLR-BA', 'MATH-BS', 'POLSC-BA', 'PSYCH-BA', 'PUBPO-BA'],
+                     'color': ['blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue'],
+                   },
+          'link': {'source': [0, 1, 1, 1, 1, 1, 1, 3],
+                   'target': [5, 2, 3, 4, 6, 7, 8, 6],
+                   'value': [103, 123, 205, 215, 302, 160, 131, 105]
+                  },
+        }
+
+    
+
     
     layout =  dict(
         title = "Basic Sankey Diagram",
